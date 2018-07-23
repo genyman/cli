bf4852337a0a6189cc94a5b6f2d1459b94a33085
Stefan de Vogelaere <stefan.de.vogelaere@gmail.com>
2018-07-23 16:28:53 +0200

    Loggin fix

--- src/Commands/NewCommand.cs
+++ src/Commands/NewCommand.cs
@@ -22,43 +22,14 @@ namespace Genyman.Cli.Commands
 		protected override int Execute()
 		{
 			var packageId = PackageIdArgument.ParsedValue;
+			var resolvePackageResult = DotNetHelper.ResolvePackage(packageId, SourceOption.ParsedValue, UpdateOption.HasValue());
 
-			var isFullPackageId = true;
-
-			if (!packageId.ToLower().Contains(".genyman."))
+			if (resolvePackageResult.success)
 			{
-				isFullPackageId = false;
-				packageId = ".genyman." + packageId;
-			}
-
-			var local = DotNetHelper.DoesPackageExists(packageId); // does check upon ENDING
-			var canContinue = false;
-
-			if (!local)
-			{
-				if (!isFullPackageId)
-				{
-					Log.Error($"Genyman package {packageId} is not installed. Auto-installation cannot be performed as {packageId} is not a fully qualified package Id.");
-					return -1;
-				}
-
-				canContinue = DotNetHelper.Install(packageId, SourceOption.ParsedValue);
-			}
-			else
-			{
-				// perform update, we need full package name
-				var latest = DotNetHelper.GetLastestPackageVersion(packageId);
-				packageId = latest.packageId; // always get full packageId here
-
-				canContinue = latest.success;
-				if (canContinue && isFullPackageId && UpdateOption.HasValue()) DotNetHelper.Update(packageId, SourceOption.ParsedValue);
-			}
-
-			if (canContinue)
-			{
-				var program = packageId;
+				var program = resolvePackageResult.packageId;
 
 				var run = ProcessRunner.Create(program)
+					.IsGenerator()
 					.WithArgument("new");
 
 				foreach (var option in Options)
@@ -72,5 +43,7 @@ namespace Genyman.Cli.Commands
 			Log.Error("Could not execute new command for this packageId.");
 			return -1;
 		}
+
+		
 	}
 }
